568e96932346495808d3445aa781938a51d09f5c
Allen Zhang <1124796703@qq.com>
2020-08-24 17:11:37 -0700

    Oh hey we have a HD version of uncut logo now yay

--- src/page/About.js
+++ src/page/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {MDBCard, MDBCardBody, MDBCardText, MDBCardTitle, MDBContainer} from "mdbreact";
+import {MDBCard, MDBCardBody, MDBCardText, MDBCardTitle, MDBCol, MDBContainer, MDBRow} from "mdbreact";
 import Footer from "../component/Footer";
 
 export default function About() {
@@ -9,13 +9,35 @@ export default function About() {
                 <MDBCardBody>
                     <MDBCardTitle cascade className="flex-center" ><h1>About Us</h1></MDBCardTitle>
                     <MDBCardBody cascade className="text-center">
-                        <MDBCardTitle>We are xxxxxx (Placeholder right now)</MDBCardTitle>
-                        <MDBCardText>
-                            We have xxx.
-                        </MDBCardText>
+                        <MDBCardTitle>We are a competitive coding organization that has different divisions when working on projects or competing</MDBCardTitle>
                     </MDBCardBody>
                 </MDBCardBody>
             </MDBCard>
+            <MDBContainer fluid className="text-center py-3">
+                <MDBRow>
+                    <MDBCol>
+                        <MDBCard>
+                            <MDBCardBody>
+                                <MDBCardTitle>Frontend</MDBCardTitle>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBCol>
+                    <MDBCol>
+                        <MDBCard>
+                            <MDBCardBody>
+                                <MDBCardTitle>Backend</MDBCardTitle>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBCol>
+                    <MDBCol>
+                        <MDBCard>
+                            <MDBCardBody>
+                                <MDBCardTitle>Hardware</MDBCardTitle>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBCol>
+                </MDBRow>
+            </MDBContainer>
             <Footer />
         </MDBContainer>
     );
